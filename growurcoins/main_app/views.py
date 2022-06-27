aa8d7edd33d25ea0e52a0084eba570b9f57b5b1c
ruchitachandhok <chandhok.ruchita@gmail.com>
2022-06-27 12:01:58 -0400

    updating details, create

--- growurcoins/main_app/views.py
+++ growurcoins/main_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Ad
 
 # Add the following import
@@ -27,8 +27,41 @@ def about(request):
     return render(request, 'about.html')
 #   return HttpResponse('<h1>About the GrowUrCoins</h1>')
 
+#controller to show all Ads
 def grow_index(request):
     ad = Ad.objects.all()
     print(ad)
     return render(request, 'growurcoins/index.html', { 'ad': ad })
+
+#controller to show specific Ad
+def grow_details(request, ad_id):
+    print("incoming wild card value is", ad_id)
+    ad = Ad.objects.get(id=ad_id)
+    print(ad)
+    print("The ad to be rendered is", ad)
+    return render(request, 'growurcoins/detail.html', {"ad": ad} )
+    # return HttpResponse("done")
+
+#controller to create a form for New Ad
+def grow_new(request):
+    return render(request, 'growurcoins/new.html')
+    # return HttpResponse("done")
+
+#controller to create a New Ad in the database
+def grow_create(request):
+     print("incoming form data", request.POST)
+     Ad.objects.create(ad_title=request.POST['ad_title'], 
+                       coins=request.POST['coins'], 
+                       description=request.POST['description'],
+                       offer_date = request.POST['offer_date'],
+                       expiry_date = request.POST['expiry_date'],
+                       stock_inventory = request.POST['stock_inventory'],
+                       picture_one = request.POST['picture_one'],
+                       category = request.POST['category'],
+                       street_number = request.POST['street_number'],
+                       street_name = request.POST['street_name'],
+                       city = request.POST['city'],
+                       postal_code = request.POST['postal_code'],
+                       )
+     return redirect('/growurcoins') 
 
